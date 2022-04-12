d35fca3d6c13c50b01e4d297ceee054f2027bab8
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-04-12 20:54:53 +0300

    aasdasdasd

--- weather/settings.py
+++ weather/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://weather-react-heroku.herokuapp.com/"
     # "http://localhost:8000",
     # "http://127.0.0.1:8000"
 ]
